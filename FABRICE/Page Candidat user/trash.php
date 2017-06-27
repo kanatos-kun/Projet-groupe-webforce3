12ba8950574a162d972731c8d8848926aab09e90
Frezzalow <fabrice_limousin@hotmail.com>
2017-06-27 14:55:04 +0200

    Dashboard V2

---
+++ FABRICE/Page Candidat user/trash.php
@@ -0,0 +1,179 @@
+<article class="news-feed__usage">  <div class="usage rc js-usage company-publish usage__company_video" data-newsid="a09f5114-7081-4a25-ab81-ad2e94708245" data-published="2017-06-21T18:03:47.690Z" data-supnewsid="2a3f774d-8333-4fcd-8070-de601ed1aa3e" data-type="company-publish-status_video" data-track-visibility="tracked" data-track-event="newsDisplayed" data-track-owneruuid="ea823e61-038f-4db7-8471-878b1f6af64d" data-track-newsid="a09f5114-7081-4a25-ab81-ad2e94708245" data-track-origin="newsfeed-company" data-url="https://www.youtube.com/watch?v=E8meIS47Bno&amp;feature=youtu.be">
+  
+  <a href="/p/002167id5vx0jzoa" class="usage__author_pic usage__main-pic img-left miniprofile-target prs" data-memberid="002167id5vx0jzoa">
+    <img src="//static8.viadeo-static.com/4FKisD38VgOqpVT-6CQDhYWRoZA=/166x166/member/002167id5vx0jzoa?ts=1470828288000" alt="Csaba Juhasz" data-track-visibility="tracked" data-track-event="memberDisplayed" data-track-displayeduuid="49919302-cee5-48a4-8e5b-c80ca17a519b" data-track-origin="newsfeed" width="70" height="70">
+  </a>
+  <div class="bx usage__box bd">
+    
+    
+    <div class="h-basic pll prl">
+      <p class="h-basic__title mbn">
+        
+        <span class="header__since fr"><span class="vicon  "></span>18 h</span>
+        
+        <a href="/p/002167id5vx0jzoa" class="usage__title miniprofile-target" data-memberid="002167id5vx0jzoa">Csaba Juhasz</a>
+        
+        <span class="header__title-action h-basic__title-action">
+          a partagé cette publication
+        </span></p>
+      </div>
+      
+      <div class="usage__body">
+        <div class="cf">
+          <div class="img-left pls mrm prxs">
+            <a href="/fr/company/intercontact-marketing-network-ltd" class="usage__company-logo dispib miniprofilecompany-target" data-trackingid="2" data-shortname="intercontact-marketing-network-ltd">
+              <img src="//static8.viadeo-static.com/WST4AUm5iZnyjz2MeUZCA3A_NWY=/fit-in/200x200/filters:fill(white)/8cd51e4e70bc43dcb92d98ea8771a848/image.jpg" class="vam" data-placement="nw-alt" alt="">
+            </a>
+          </div>
+          
+          <div class="usage_description bd">
+            <p class="man">
+              <a class="usage__title usage__member-name" href="/fr/company/intercontact-marketing-network-ltd">Intercontact Marketing Network Ltd</a>
+            </p>
+            <p class="man">
+            </p><p class="header__since"><span class="vicon  "></span>21/06/2017</p>
+            <p></p>
+          </div>
+        </div>
+        
+        
+        <p class="mbn mtm">Jupiter Icy Moons Orbiter (from James Cameron's Aliens of the Deep) <a href="https://youtu.be/E8meIS47Bno" target="_blank">https://youtu.be/E8meIS47Bno</a></p>
+        
+        <div class="ptl">
+          <div class="rc usage-article relative">
+           <a class=" usage-article__link relative" href="https://www.youtube.com/watch?v=E8meIS47Bno&amp;feature=youtu.be" alt="Aller à Jupiter Icy Moons Orbiter (from James Cameron's Aliens of the Deep) (Part 2)" target="_blank">
+            <div class="layered loader-overlay"></div>
+            <img src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" style="background-image: url(//static8.viadeo-static.com/a-V-yR3CEF3SggQAYh9AzVAre7k=/185x185/smart/079c353443f744d7bd9313823caffd48/image.jpg);" class="usage-article__image img img-left" alt="" width="100%">
+            
+            <div class="usage-article__text pll bd">
+              <div class="usage-article__border">
+                <h3 class="usage-article__title pts mbn">Jupiter Icy Moons Orbiter (from James Cameron's Aliens of the Deep) (Part 2)</h3>
+                
+                
+                <p class="usage-article__description pts mbn">A clip from James' Cameron's Aliens of the Deep highlighting the canceled Jupiter Icy Moons Orbiter's (JIMO) mission to explore the Jovian moons and send a probe to explore the oceans under Europa's I…</p>
+                
+                
+                
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+      
+      
+    </div>
+    
+    <div class="usage__footer-basic-actions cf pll prl ptxs js-usage-footer">
+      <div class="fl">
+        
+        <a href="#" class="js-do-like usage__like-action ">
+          <span>J'aime</span>
+        </a>
+        <a href="#" class="js-do-unlike usage__unlike-action hidden">
+          <span>Vous aimez</span>
+        </a>
+        <div class="dd">
+          <a class="dd-btn pll prn pls-s usage__share-action">Partager<span class="vicon vicon-arrow-down"></span></a>
+          <ul class="dd-menu js-usage__info-share">
+            <li class="dd-item">
+              <div class="pbxs usage__info-share-rc-default js-usage__info-share-rc-default tal pam ">
+                <button type="button" class="vam btn-link usage__info-share-cta-title-header js-newsfeed-shareNews" data-origin="newsshare">
+                  <span class="vicon vicon-share vam"></span>
+                  <span class="mlxs usage__info-share-cta-title vam">Partager sur Viadeo</span>
+                  
+                </button>
+                <div class="ptxs usage__info-share-cta-text prxs plxs pbxs">Ce contenu sera partagé avec vos contacts</div>
+              </div>
+              
+              <div class="hidden pbxs usage__info-share-rc-feedback js-usage__info-share-rc-feedback tal pam">
+                <button type="button" class="vam btn-link usage__info-share-cta-title-header">
+                  <span class="vicon vicon-share vam"></span>
+                  <span class="mlxs usage__info-share-cta-title">Partage réussi</span>
+                  
+                </button>
+                <div class="ptxs usage__info-share-cta-text prxs plxs pbxs">Ce contenu a bien été partagé avec vos contacts</div>
+              </div>
+            </li>
+          </ul>
+        </div>
+        
+      </div>
+      
+      <ul class="usage__footer-actions-counters">
+        <li class="fl prs mrxs prxs-s mrn-s js-like-ico ">
+          <span class="vicon vicon-like mrxs mrn-s"></span>
+          <span class="js-likers-total">1</span>
+        </li>
+        <li class="js-numberOfShares-container fl prs mrxs prxs-s mrn-s">
+          <span class="vicon vicon-share mrxs mrn-s"></span><span class="js-shares-total">1</span></li>
+          
+        </ul>
+        
+      </div>
+      
+      <div class="js-footer-likers-container"><div>
+        <div class="usage__grey-element usage__footer-likers cf">
+
+          <div class="pts mll rc">
+
+            <div class="img-left">
+
+
+              <a href="/p/002167id5vx0jzoa" class="fl prxs miniprofile-target" data-memberid="002167id5vx0jzoa">
+                <img src="https://static8.viadeo-static.com/yNRMbxfQ4xqSDzZFEVNGIv2rL0k=/80x80/member/002167id5vx0jzoa?ts=1470828288000" alt="" class="usage__likers-pic" width="28" height="28">
+              </a>
+
+
+            </div>
+
+            <div class="bd pls prs ptxs">
+
+              <div class="usage__footer-likers-authors">
+
+                <a href="/p/002167id5vx0jzoa">Csaba Juhasz</a>
+                
+
+
+                aime cela
+
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div></div>
+      
+      <div class="usage__grey-element js-comments-container"><div><ul class="usage__comments-list js-comments-list"></ul>
+
+        <div class="js-add-comment-container usage__form-comment usage__no-comment">
+
+          <div class="rc pts mll">
+
+            <div class="img-left mrs">
+              <img src="//static8.viadeo-static.com/1I2LX6zP82IDR3oBIM2nN-Y_De8=/80x80/member/0021a1vktzlsou5n?ts=1402327981000" alt="" class="usage__comment-pic" width="28" height="28">
+            </div>
+
+            <div class="bd">
+              <form class="js-comment-form post-comment__form post-comment__form-collapsed core-form cf">
+                <div class="cf">
+                  <textarea name="comment" class="post-comment__input js-comment-input mbn dispb fl" placeholder="Ajouter un commentaire"></textarea>
+
+                  <button class="js-comment-submit post-comment__submit btn btn-primary" type="submit">Commenter</button>
+                </div>
+
+                <div class="js-comment-length post-comment-length cf">0</div>
+              </form>
+            </div>
+
+          </div>
+
+        </div>
+      </div></div>
+      
+    </div>
+  </div>
+</article>
