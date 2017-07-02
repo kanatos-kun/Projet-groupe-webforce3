a3645f4a41659cf5b77b49b35c12f8ab23f27794
Geoffrey Arnoult <geoffreydu89@gmail.com>
2017-07-02 09:07:47 +0200

    Envoye le dossier prototype dans le dossier KANATOS

---
+++ KANATOS/PROTOTYPE/frameworkw/app/routes.php
@@ -0,0 +1,11 @@
+<?php
+
+	$w_routes = array(
+		['GET|POST', '/', 'Default#home', 'default_home'],
+		['GET|POST', '/projet','default#projet','default_projet'],
+		['GET|POST', '/profil','default#profil','default_profil'],
+		['GET|POST', '/propos','default#propos','default_propos'],
+		['GET|POST', '/premium','default#premium','default_premium'],
+		['GET|POST', '/dashboard','default#dashboard','default_dashboard'],
+		['GET|POST', '/formation','default#formation','default_formation']
+	);
